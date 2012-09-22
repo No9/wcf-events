e14a97d8968d94b5f1fb7d8908aaeafdb0ba0289
No9 <mrcowsy@gmail.com>
2012-09-22 21:41:08 +0100

    Added cluster to middleware

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 var announce = require('socket.io-announce').createClient();
-var actions = [];
-var simpleaction = {};
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 process.setMaxListeners(5000);
-// Select a node by its class name. You can also select by tag e.g. 'div'
-simpleaction.query = 'GetDataResult';
 
-// Create an function that is executed when that node is selected. Here we just replace '& frames' with '+trumpet' 
-simpleaction.func = function (node) {
-                            node.html(function (html) {
-								console.log(node.name + ' = ' + html);
-								announce.emit( 'cdcevent', { "item" : html } );
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {// Select a node by its class name. You can also select by tag e.g. 'div'
+	
+	var actions = [];
+	var simpleaction = {};
+	simpleaction.query = 'GetDataResult';
+
+	// Create an function that is executed when that node is selected. Here we just replace '& frames' with '+trumpet' 
+	simpleaction.func = function (node) {
+								node.html(function (html) {
+									console.log(node.name + ' = ' + html);
+									announce.emit( 'cdcevent', { "item" : html } );
+									return;
+								});
 								return;
-							});
-							return;
-                        };
-                     
-// Add the action to the action array
-actions.push(simpleaction);
+							};
+						 
+	// Add the action to the action array
+	actions.push(simpleaction);
+
+	var harmon = require('harmon').harmon(actions) 
+	var proxyoptions = { router: 'table.json' };
+	require('./lib/appproxy.js').generate(proxyoptions, harmon);
+}
 
 
-var harmon = require('harmon').harmon(actions) 
-var proxyoptions = { router: 'table.json' };
-require('./lib/appproxy.js').generate(proxyoptions, harmon);
